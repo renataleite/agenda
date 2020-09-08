d8dd102e2c5b5e1f16047046760e50929676dad9
RENATA GUEDES <renataleitesg@gmail.com>
2020-09-08 11:08:24 +0100

    cria o projeto de teste, utilizando moq

---
+++ Agenda.Repos.Test/ClasRepositorioContatosTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agenda.DAL;
+using Agenda.Repos;
+using Agenda.Domain;
+using NUnit.Framework;
+using Moq;
+
+namespace Agenda.Repos.Test
+{
+    [TestFixture]
+    public class ClasRepositorioContatosTest
+    {
+        Mock<IContatos> _contatos;
+        Mock<ITelefones> _telefones;
+        RepositorioContatos _repositorioContatos;
+
+        [SetUp]
+        public void Setup()
+        {
+            _contatos = new Mock<IContatos>();
+            _telefones = new Mock<ITelefones>();
+            _repositorioContatos = new RepositorioContatos(_contatos.Object, _telefones.Object);
+        }
+
+        [Test]
+        public void Test()
+        {
+
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+    }
+}
