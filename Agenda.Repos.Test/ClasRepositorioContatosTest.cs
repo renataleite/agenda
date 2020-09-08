43b1298cad1383096e5f631fa8263ccbec683e3e
RENATA GUEDES <renataleitesg@gmail.com>
2020-09-08 22:23:27 +0100

    testa repositório com mock

--- Agenda.Repos.Test/ClasRepositorioContatosTest.cs
+++ Agenda.Repos.Test/ClasRepositorioContatosTest.cs
@@ -29,21 +29,44 @@ namespace Agenda.Repos.Test
         [Test]
         public void DeveSerPossivelObterContatoComListaTelefone()
         {
+            Guid telefoneId = Guid.NewGuid();
+            Guid contatoId = Guid.NewGuid();
+            List<ITelefone> lstTelefone = new List<ITelefone>();
             //Monta
-                //Criar Moq de IContato
-                //Moq da função ObterPorId de IContatos
-                //Criar Moq de Itelefone
-                //Moq da funcao ObterTodosDoContato de Itelefones
+            //Criar Moq de IContato
+            Mock<IContato> mContato = new Mock<IContato>();
+            mContato.SetupGet(o => o.Id).Returns(contatoId);
+            mContato.SetupGet(o => o.Nome).Returns("João");
+            mContato.SetupSet(o => o.Telefones = It.IsAny<List<ITelefone>>())
+                .Callback<List<ITelefone>>(p => lstTelefone = p);
+            //Moq da função ObterPorId de IContatos
+            _contatos.Setup(o => o.Obter(contatoId)).Returns(mContato.Object);
+            //Criar Moq de Itelefone
+            Mock<ITelefone> mTelefone = new Mock<ITelefone>();
+            mTelefone.SetupGet(o => o.Id).Returns(telefoneId);
+            mTelefone.SetupGet(o => o.Numero).Returns("1234-1234");
+            mTelefone.SetupGet(o => o.ContatoId).Returns(contatoId);
+            //Moq da funcao ObterTodosDoContato de Itelefones
+            _telefones.Setup(o => o.ObterTodosDoContato(contatoId)).Returns(new List<ITelefone> { mTelefone.Object });
             //Excuta
-                //chama o metodo ObterPorId de RepositorioContatos
+            //chama o metodo ObterPorId de RepositorioContatos
+            IContato contatoResultado = _repositorioContatos.ObterPorId(contatoId);
+            mContato.SetupGet(o => o.Telefones).Returns(lstTelefone);
             //Verifica
-                //Verificar se o Conato retornado contém os mesmos dados do Moq Icontato com a lista de Telefones do Moq Itelefone
-
+            //Verificar se o Conato retornado contém os mesmos dados do Moq Icontato com a lista de Telefones do Moq Itelefone
+            Assert.AreEqual(mContato.Object.Id, contatoResultado.Id);
+            Assert.AreEqual(mContato.Object.Nome, contatoResultado.Nome);
+            Assert.AreEqual(1, contatoResultado.Telefones.Count);
+            Assert.AreEqual(mTelefone.Object.Numero, contatoResultado.Telefones[0].Numero);
+            Assert.AreEqual(mTelefone.Object.Id, contatoResultado.Telefones[0].Id);
+            Assert.AreEqual(mContato.Object.Id, contatoResultado.Telefones[0].ContatoId);
         }
 
         [TearDown]
         public void TearDown()
         {
+            _contatos = null;
+            _telefones = null;
 
         }
 
