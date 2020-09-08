bbdc21eae9c1a9b68d4e0304ba6db3a1049ab6ed
RENATA GUEDES <renataleitesg@gmail.com>
2020-09-08 10:51:30 +0100

    cria o projeto repos e interfaces

---
+++ Agenda.Repos/RepositorioContatos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agenda.DAL;
+
+namespace Agenda.Repos
+{
+    public class RepositorioContatos
+    {
+        private readonly IContatos _contatos;
+        private readonly ITelefones _telefones;
+
+        public RepositorioContatos(IContatos contatos, ITelefones telefones)
+        {
+            _contatos = contatos;
+            _telefones = telefones;
+        }
+    }
+}
