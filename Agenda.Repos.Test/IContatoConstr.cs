a746c03e40fac55042fbebb3b4638b46e3f7b222
Renata Guedes <renataleitesg@gmail.com>
2020-10-12 08:37:09 +0100

    test

---
+++ Agenda.Repos.Test/IContatoConstr.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Moq;
+using AutoFixture;
+using Agenda.Domain;
+
+namespace Agenda.Repos.Test
+{
+    class IContatoConstr
+    {
+        private readonly Mock<IContato> _mockIContato;
+        private readonly Fixture _fixture;
+
+        protected IContatoConstr(Mock<IContato> mockIContato, Fixture fixture)
+        {
+            _mockIContato = mockIContato;
+            _fixture = fixture;
+        }
+
+        public static IContatoConstr Um()
+        {
+            return new IContatoConstr(new Mock<IContato>(), new Fixture());
+        }
+
+        public IContato Construir()
+        {
+            return _mockIContato.Object;
+        }
+
+        public Mock<IContato> Obter()
+        {
+            return _mockIContato;
+        }
+
+        public IContatoConstr ComNome(string nome)
+        {
+            _mockIContato.SetupGet(o => o.Nome).Returns(nome);
+            return this;
+        }
+        public IContatoConstr ComId(Guid id)
+        {
+            _mockIContato.SetupGet(o => o.Id).Returns(id);
+            return this;
+        }
+    }
+}
---
+++ Agenda.Repos.Test/ITelefoneConstr.cs
@@ -0,0 +1,56 @@
+﻿using Agenda.Domain;
+using Moq;
+using System;
+using AutoFixture;
+
+namespace Agenda.Repos.Test
+{
+    class ITelefoneConstr
+    {
+        protected readonly Mock<ITelefone> _mockTelefone;
+        protected readonly Fixture _fixture;
+
+        protected ITelefoneConstr(Mock<ITelefone> mockTelefone, Fixture fixture)
+        {
+            _mockTelefone = mockTelefone;
+            _fixture = fixture;
+        }
+
+        public static ITelefoneConstr Um()
+        {
+            return new ITelefoneConstr(new Mock<ITelefone>(), new Fixture());
+        }
+
+        public ITelefone Construir()
+        {
+            return _mockTelefone.Object;
+        }
+
+        public ITelefoneConstr Padrao()
+        {
+            _mockTelefone.SetupGet(o => o.Id).Returns(_fixture.Create<Guid>());
+            _mockTelefone.SetupGet(o => o.Numero).Returns(_fixture.Create<string>());
+            _mockTelefone.SetupGet(o => o.Id).Returns(_fixture.Create<Guid>());
+            return this;
+        }
+
+        public ITelefoneConstr ComId(Guid id)
+        {
+            _mockTelefone.SetupGet(o => o.Id).Returns(id);
+            return this;
+        }
+        public ITelefoneConstr ComNumero(string numero)
+        {
+            _mockTelefone.SetupGet(o => o.Numero).Returns(numero);
+            return this;
+        }
+        public ITelefoneConstr ComContatoId(Guid contatoId)
+        {
+            _mockTelefone.SetupGet(o => o.Id).Returns(contatoId);
+            return this;
+        }
+        //mTelefone.SetupGet(o => o.Id).Returns(telefoneId);
+        //mTelefone.SetupGet(o => o.Numero).Returns("1234-1234");
+        //mTelefone.SetupGet(o => o.ContatoId).Returns(contatoId);
+    }
+}
